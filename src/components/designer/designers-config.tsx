0e5370c7f456e9eb92f11f15c50d520338232b9e
Max Zinovik <zinovik@gmail.com>
2020-09-03 23:18:30 +0300

    add designer component

---
+++ src/components/designer/designers-config.tsx
@@ -0,0 +1,106 @@
+import { flags } from './flags';
+
+export const designersConfig: {
+  [key: string]: { text: string; link: string };
+} = {
+  'alexander-pfister': {
+    text: `${flags.AT} Alexander Pfister`,
+    link: '11767/alexander-pfister',
+  },
+  'ananda-gupta': {
+    text: `${flags.US} Ananda Gupta`,
+    link: '3876/ananda-gupta',
+  },
+  'andreas-seyfarth': {
+    text: `${flags.DE} Andreas Seyfarth`,
+    link: '117/andreas-seyfarth',
+  },
+  'antoine-bauza': {
+    text: `${flags.FR} Antoine Bauza`,
+    link: '9714/antoine-bauza',
+  },
+  'bruno-cathala': {
+    text: `${flags.FR} → ${flags.CH} Bruno Cathala`,
+    link: '1727/bruno-cathala',
+  },
+  'dan-cassar': {
+    text: `${flags.US} Dan Cassar`,
+    link: '53889/dan-cassar',
+  },
+  'daniele-tascini': {
+    text: `${flags.IT} Daniele Tascini`,
+    link: '59986/daniele-tascini',
+  },
+  'darwin-kastle': {
+    text: `${flags.US} Darwin Kastle`,
+    link: '4059/darwin-kastle',
+  },
+  'eric-m-lang': {
+    text: `${flags.CA} Eric M. Lang`,
+    link: '1533/eric-m-lang',
+  },
+  'jamey-stegmaier': {
+    text: `${flags.US} Jamey Stegmaier`,
+    link: '62640/jamey-stegmaier',
+  },
+  'jason-matthews': {
+    text: `${flags.US} Jason Matthews`,
+    link: '3877/jason-matthews',
+  },
+  'john-yianni': {
+    text: `${flags.GB} John Yianni`,
+    link: '1078/john-yianni',
+  },
+  'kota-nakayama': {
+    text: `${flags.JP} Kota Nakayama`,
+    link: '74989/kota-nakayama',
+  },
+  'martin-wallace': {
+    text: `${flags.GB} → ${flags.NZ} → ${flags.AU} Martin Wallace`,
+    link: '6/martin-wallace',
+  },
+  'reiner-knizia': {
+    text: `${flags.DE} → ${flags.GB} Reiner Knizia`,
+    link: '2/reiner-knizia',
+  },
+  'rob-daviau': {
+    text: `${flags.US} Rob Daviau`,
+    link: '442/rob-daviau',
+  },
+  'robert-dougherty': {
+    text: `${flags.US} Robert Dougherty`,
+    link: '4113/robert-dougherty',
+  },
+  'sebastien-pauchon': {
+    text: `${flags.CH} Sébastien Pauchon`,
+    link: '6736/sebastien-pauchon',
+  },
+  'simone-luciani': {
+    text: `${flags.IT} Simone Luciani`,
+    link: '35418/simone-luciani',
+  },
+  'stefan-bruck': {
+    text: `Stefan Brück`,
+    link: '2820/stefan-bruck',
+  },
+  'stefan-feld': {
+    text: `${flags.DE} Stefan Feld`,
+    link: '4958/stefan-feld',
+  },
+  'thomas-lehmann': {
+    text: `${flags.US} Thomas Lehmann`,
+    link: '150/thomas-lehmann',
+  },
+  'uwe-rosenberg': {
+    text: `${flags.DE} Uwe Rosenberg`,
+    link: '10/uwe-rosenberg',
+  },
+  'vital-lacerda': {
+    text: `${flags.PT} Vital Lacerda`,
+    link: '12396/vital-lacerda',
+  },
+  'vlaada-chvatil': {
+    text: `${flags.CZ} Vlaada Chvátil`,
+    link: '789/vlaada-chvatil',
+  },
+};
---
+++ src/components/designer/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { designersConfig } from './designers-config';
+
+const SOME_HIGH_RATED_GAMES = 'Some high-rated games';
+
+type DesignerProps = {
+  name: string;
+  isText?: boolean;
+  isPageLink?: boolean;
+  isGames?: boolean;
+};
+
+const Designer: React.FC<DesignerProps> = ({
+  name,
+  isText,
+  isPageLink,
+  isGames,
+}) => {
+  const designer = designersConfig[name] || {};
+
+  return isText ? (
+    <>{designer.text}</>
+  ) : isPageLink ? (
+    <a href={`#${name}`}>{designer.text}</a>
+  ) : (
+    <a
+      href={`https://boardgamegeek.com/boardgamedesigner/${designer.link}${
+        isGames ? '/linkeditems/boardgamedesigner?pageid=1&sort=rank' : ''
+      }`}
+      name={name}
+      target="_blank"
+    >
+      {isGames ? SOME_HIGH_RATED_GAMES : designer.text}
+    </a>
+  );
+};
+
+export default Designer;
