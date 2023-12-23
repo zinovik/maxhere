42531cc126145cdc10600531cfbad1cf5a2f72ac
Max Zinovik <zinovik@gmail.com>
2023-12-23 18:30:19 +0100

    remove ranks update functionality

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { BggGamesContext } from '../templates/blog-post';
 
 type GameProps = {
@@ -10,15 +10,8 @@ type GameProps = {
   isText?: boolean;
 };
 
-const DEFAULT_LABEL: string = 'update ranks';
-const UPDATING_LABEL: string = 'updating...';
-const ERROR_UPDATING_LABEL: string = 'error updating!';
-const UPDATED_LABEL: string = 'updated!';
-
 const BGG_GAMES_RANKS_STATIC =
   'https://raw.githubusercontent.com/zinovik/bgg-games-ranks-data/main/bgg-games-ranks.json';
-const BGG_GAMES_RANKS_FUNCTION =
-  'https://bgg-games-ranks-parser-wniawguk3a-uc.a.run.app?amount=2000';
 
 const Game: React.FC<GameProps> = ({
   isDateOnly,
@@ -27,56 +20,21 @@ const Game: React.FC<GameProps> = ({
   isSkipYear,
   isText,
 }) => {
-  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-  const [buttonLabel, setButtonLabel] = useState(DEFAULT_LABEL);
-
   const { bggGames, setBggGames } = useContext(BggGamesContext);
 
   useEffect(() => {
     axios.get(BGG_GAMES_RANKS_STATIC).then(({ data }) => setBggGames(data));
   }, []);
 
-  const setTemporaryLabel = (label: string) => {
-    setButtonLabel(label);
-
-    setTimeout(() => {
-      setButtonLabel(DEFAULT_LABEL);
-      setIsButtonDisabled(false);
-    }, 10000);
-  };
-
   if (!bggGames) {
     return <></>;
   }
-
-  const updateBgg = async () => {
-    setIsButtonDisabled(true);
-    setButtonLabel(UPDATING_LABEL);
-
-    try {
-      const { data: response } = await axios.get(BGG_GAMES_RANKS_FUNCTION);
-
-      if (!response) {
-        throw new Error(ERROR_UPDATING_LABEL);
-      }
-
-      setBggGames(response);
-      setTemporaryLabel(UPDATED_LABEL);
-    } catch (error) {
-      setTemporaryLabel(error.message);
-    }
-  };
 
   if (isDateOnly) {
     return (
       <>
         {new Date(bggGames.date).toLocaleDateString()}
         <br />
-        <>
-          <button onClick={updateBgg} disabled={isButtonDisabled}>
-            {buttonLabel}
-          </button>
-        </>
       </>
     );
   }
